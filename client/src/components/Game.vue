8087e65f8b2a1f463de3640583af13dd85f297ae
fznakbar <fauzanakbar10@gmail.com>
2020-03-20 06:12:41 +0700

    add new style for game room

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -1,24 +1,25 @@
 <template>
-  <div>
+  <div id="game">
+    <h3 style="color:white; text-align:center" class="mt-3 mb-3" >Your Card</h3>
     <b-container>
-      <b-card >
-      <div class="d-flex justify-content-center">
+      <b-card style="background-color:rgba(0, 0, 0, 0.438)">
+      <div class="d-flex justify-content-center" >
         <div v-for="card in cards" :key="card.id">
-          <b-card>
+          <b-card style="background-color:rgba(0, 0, 0, 0.438)">
             <b-img class="kartu" :src="card.image">
             </b-img>
           </b-card>
         </div>
       </div>
       </b-card>
-      <b-card>
+      <b-card  style="background-color:rgba(0, 0, 0, 0.438)">
         <div class="d-flex justify-content-center">
         <b-button variant="primary" class="mx-1" v-if="gabolehlagi" @click="drawCard()">Draw</b-button>
         <b-button variant="danger" class="mx-1" v-if="gabolehlagi" @click="done()">Done</b-button>
         </div>
       </b-card>
       <b-card>
-        <h1 class="text-center">{{points}}</h1>
+        <h1 class="text-center">Total Value : {{points}}</h1>
       </b-card>
     </b-container>
   </div>
@@ -84,4 +85,15 @@ export default {
   .kartu {
     width: 150px;
   }
+  #game {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #8E0E00;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #1F1C18, #8E0E00);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #1F1C18, #8E0E00); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  /* background-image: url('../assets/lobby.jpg') */
+}
 </style>
