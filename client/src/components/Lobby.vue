5eea26100d9d93baf850764252246dfe2779395c
Arif Rachman Husen <arif05rachman>
2020-03-19 22:56:33 +0700

    add components, router

---
+++ client/src/components/Lobby.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <div class="d-flex justify-content-center my-4">
+    <b-card>
+      <b-form>
+        <b-form-input v-model="text" placeholder="Create New Room"></b-form-input>
+        <div class="d-flex justify-content-center mt-2">
+          <b-button>Create</b-button>
+        </div>
+      </b-form>
+    </b-card>
+  </div>
+  <div>
+    <b-card>
+      <b-card class="">
+        <b-card title="Card Title" tag="Room" style="width: 20rem;" class="mb-2">
+          <div class="d-flex justify-content-center">
+            <b-button href="#" variant="primary">Enter</b-button>
+          </div>
+        </b-card>
+      </b-card>
+    </b-card>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Lobby",
+}
+</script>
+
+<style>
+
+</style>
