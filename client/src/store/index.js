eca9ae343c7faabc898f8315389cab0b960bac67
Arif Rachman Husen <arif05rachman>
2020-03-19 23:46:03 +0700

    get all roms

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,11 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    rooms: null
   },
   mutations: {
+    rooms(state, payload) {
+      state.rooms = payload;
+    }
   },
   actions: {
+    getRooms(context) {
+      this.$axios({
+        url: "/rooms",
+        method: "post"
+      })
+        .then(({ data }) => {
+        context.commit("rooms", data)
+        })
+        .catch(({ response }) => {
+        console.log(response)
+      })
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
